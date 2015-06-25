8e6555e749a7448ee5979fc9cc043b1e9c552785
Donald Nairn <donald.nairn@gmail.com>
2015-06-25 23:13:56 +0100

    Missing some files... what's wrong with GitHub's app.

--- dist/handsontable.full.js
+++ dist/handsontable.full.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Thu Jun 25 2015 22:56:21 GMT+0100 (GMT Daylight Time)
+ * Date: Thu Jun 25 2015 23:07:13 GMT+0100 (GMT Daylight Time)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.15.0',
-  buildDate: 'Thu Jun 25 2015 22:56:21 GMT+0100 (GMT Daylight Time)'
+  buildDate: 'Thu Jun 25 2015 23:07:13 GMT+0100 (GMT Daylight Time)'
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
